4279d56cd6d1a76f24fefe746448a0e293b1a5ef
Lucas Lambert <lucaslydon@Lucass-MacBook-Pro.local>
2021-10-11 01:27:14 -0400

    testing env

--- config.js
+++ config.js
@@ -7,6 +7,11 @@ const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 
 const PORT = +process.env.PORT || 3001;
 
+const PORT_NUMBER = process.env.PORT_NUMBER;
+
+
+console.log(PORT_NUMBER);
+
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
