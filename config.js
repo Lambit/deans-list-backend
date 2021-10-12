fdecc6a7d53b22443ce33d23acab7c31531b5837
Lucas Lambert <lucaslydon@Lucass-MacBook-Pro.local>
2021-10-11 23:12:49 -0400

    double checking variables

--- config.js
+++ config.js
@@ -9,16 +9,14 @@ const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 
 const BASE_URL = process.env.BASE_URL;
 
-
 // development
-const SANDBOX_ACCESS_TOKEN = process.env.SANDBOX_ACCESS_TOKEN;
-const SANDBOX_APPLICATION_ID = process.env.SANDBOX_APPLICATION_ID;
+const SAND_ACCESS = process.env.SANDBOX_ACCESS_TOKEN;
+const SAND_APP_ID = process.env.SANDBOX_APPLICATION_ID;
 
-console.log('config-token:', SANDBOX_ACCESS_TOKEN, 'config-id:', SANDBOX_APPLICATION_ID);
 
 // production
-const SQUARE_APPLICATION_ID = process.env.SQUARE_APPLICATION_ID;
-const SQUARE_ACCESS_TOKEN = process.env.SQUARE_ACCESS_TOKEN;
+const SQ_APP_ID = process.env.SQUARE_APPLICATION_ID;
+const SQ_TOKEN = process.env.SQUARE_ACCESS_TOKEN;
 
 // location for dev and pro
 const LOCATION_ID = process.env.LOCATION_ID;
@@ -37,7 +35,6 @@ const BCRYPT_WORK_FACTOR = 12;
 
 console.log("DB Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
 console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
@@ -45,11 +42,10 @@ console.log("---");
 module.exports = {
   SECRET_KEY,
   BASE_URL,
-  API,
-  SANDBOX_ACCESS_TOKEN,
-  SANDBOX_APPLICATION_ID,
-  SQUARE_APPLICATION_ID,
-  SQUARE_ACCESS_TOKEN,
+  SAND_ACCESS,
+  SAND_APP_ID,
+  SQ_APP_ID,
+  SQ_TOKEN,
   LOCATION_ID,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri, 
